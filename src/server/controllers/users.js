4601cf60f3231d360d0fd569cea5fe0e3cce5f05
Bruno Santos <bruno1pb13@gmail.com>
2023-03-24 10:06:59 -0300

    criar e ativar conta

--- src/server/controllers/users.js
+++ src/server/controllers/users.js
@@ -1,28 +1,106 @@
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient, Prisma  } = require('@prisma/client')
 const prisma = new PrismaClient()
-
+const DEBUG = require('../middlewares/log')
+const bcrypt = require('bcrypt')
 
 async function createNewUser(data){
 
     try{
-        console.log(data)
 
-        const { name, email, password } = data;
+        const { name, email} = data;
+
+        let {password} = data
+        password = await cryptPassword(password)
+
         const user = await prisma.user.create({
             data: {
                 name,
                 email,
                 password
+            },
+            select: {
+                id: true,
+                name: true,
+                status: true,
             }
         })
+        console.log(user)
+
+        //create activation code
+        if(user){
+
+            const token = Math.floor(100000000000 + Math.random() * 900000000000)
+
+            const code = await prisma.activationCode.create({
+                data: {
+                    email: email,
+                    code: String(token)
+                }
+            })
+
+            console.log(code)
+        }
         
 
+        DEBUG('sr-only', user)
         return(user)
 
+
     }catch(err){
-        res.status(400).json({error: 'Error creating new user'})
+        if(err instanceof Prisma.PrismaClientKnownRequestError){
+            if (err.code === 'P2002') {
+                throw new Error(`There is a unique constraint violation, a new user cannot be created with this ${err.meta.target}`)
+            }
+        }
+
+        throw new Error(err)
+       
     }
 
 }
 
-module.exports = {createNewUser}
+async function activeUser(data){
+    try{
+        const { email, code } = data;
+
+    //check if code is valid
+    const user = await prisma.activationCode.findUnique({
+        where: {
+            email: email,
+        }
+    })
+
+    if(user.code !== code){
+        throw new Error('Invalid code')
+    }
+
+    //update user status
+    const updatedUser = await prisma.user.update({
+        where: {
+            email: email
+        },
+        data: {
+            status: 'active'
+        },
+        select: {
+            id: true,
+            name: true,
+            status: true,
+            email: true
+        }
+    })
+
+    return updatedUser
+
+    }catch(err){
+        throw err
+    }
+}
+
+async function cryptPassword(password){
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+}
+
+module.exports = {createNewUser, activeUser}
--- src/server/router/user.js
+++ src/server/router/user.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const router = express.Router();
-
+const DEBUG = require('../middlewares/log')
 const USER = require('../controllers/users')
 
 router.put('/', async (req,res)=>{
     try{
         let user = await USER.createNewUser(req.body)
-        if(!user){
-            res.status(400).json({error: 'Error creating new user'})
-        }
+        
+        DEBUG('sr-only', user)
+        
         res.json(user)
     }catch(err){
+        DEBUG('error', err)
+        res.status(409).send(String(err))
+    }
+})
 
+router.get('/active', async (req,res)=>{
+    try{
+
+        let active = await USER.activeUser(req.query)
+
+        if(active){
+            return res.status(200).send(active)
+        }
+
+    }catch(err){
+        DEBUG('error', err)
+        res.sendStatus(400)
     }
 })
 
