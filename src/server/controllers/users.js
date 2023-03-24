91757dda30d775103dbc379def84cc4a4ed0627d
Bruno Santos <bruno1pb13@gmail.com>
2023-03-24 13:58:25 -0300

    Finalizado sistema de criação e gerenciamento de usuario

--- src/server/controllers/users.js
+++ src/server/controllers/users.js
@@ -1,17 +1,18 @@
-const { PrismaClient, Prisma  } = require('@prisma/client')
+const { PrismaClient, Prisma } = require('@prisma/client')
 const prisma = new PrismaClient()
 const DEBUG = require('../middlewares/log')
 const bcrypt = require('bcrypt')
 
-async function createNewUser(data){
+async function createNewUser(data) {
 
-    try{
+    try {
 
-        const { name, email} = data;
+        const { name, email } = data;
 
-        let {password} = data
+        let { password } = data
         password = await cryptPassword(password)
 
+        //create user
         const user = await prisma.user.create({
             data: {
                 name,
@@ -24,15 +25,20 @@ async function createNewUser(data){
                 status: true,
             }
         })
-        console.log(user)
 
         //create activation code
-        if(user){
+        if (user) {
 
             const token = Math.floor(100000000000 + Math.random() * 900000000000)
 
-            const code = await prisma.activationCode.create({
-                data: {
+            const code = await prisma.activationCode.upsert({
+                where: {
+                    email: email,
+                },
+                update: {
+                    code: String(token)
+                },
+                create: {
                     email: email,
                     code: String(token)
                 }
@@ -40,67 +46,181 @@ async function createNewUser(data){
 
             console.log(code)
         }
-        
+
 
         DEBUG('sr-only', user)
-        return(user)
+        return (user)
 
 
-    }catch(err){
-        if(err instanceof Prisma.PrismaClientKnownRequestError){
+    } catch (err) {
+        if (err instanceof Prisma.PrismaClientKnownRequestError) {
             if (err.code === 'P2002') {
                 throw new Error(`There is a unique constraint violation, a new user cannot be created with this ${err.meta.target}`)
             }
         }
 
         throw new Error(err)
-       
+
     }
 
 }
 
-async function activeUser(data){
-    try{
+async function activeUser(data) {
+    try {
         const { email, code } = data;
 
-    //check if code is valid
-    const user = await prisma.activationCode.findUnique({
-        where: {
-            email: email,
-        }
-    })
+        //check if code is valid
+        const user = await prisma.activationCode.findUnique({
+            where: {
+                email: email,
+            }
+        })
 
-    if(user.code !== code){
-        throw new Error('Invalid code')
+        if (user.code !== code) {
+            throw new Error('Invalid code')
+        }
+
+        await prisma.activationCode.delete({
+            where: {
+                email: email
+            }
+        })
+
+        //update user status
+        const updatedUser = await prisma.user.update({
+            where: {
+                email: email
+            },
+            data: {
+                status: 'active'
+            },
+            select: {
+                id: true,
+                name: true,
+                status: true,
+                email: true
+            }
+        })
+
+
+
+        return updatedUser
+
+    } catch (err) {
+        throw err
     }
+}
 
-    //update user status
-    const updatedUser = await prisma.user.update({
-        where: {
-            email: email
-        },
-        data: {
-            status: 'active'
-        },
-        select: {
-            id: true,
-            name: true,
-            status: true,
-            email: true
-        }
-    })
+async function banUser(email){
+    try{
+        const user = await prisma.user.update({
+            where:{
+                email: email
+            },
+            data:{
+                status: 'blocked'
+            },
+            select:{
+                id: true,
+                status: true,
+            }
+        })
 
-    return updatedUser
+        return user
+    }catch(err){
+        throw err
+    }
+}
+
+async function unbanUser(email){
+    try{
+        const user = await prisma.user.update({
+            where:{
+                email: email
+            },
+            data:{
+                status: 'active'
+            },
+            select:{
+                id: true,
+                status: true,
+            }
+        })
+
+        return user
+    }catch(err){
+        throw err
+    }
+}
+
+async function generateRecoveryPasswordToken(email){
+    try{
+        
+        const token = Math.floor(100000000000 + Math.random() * 900000000000)
+
+        await prisma.RecoveryPassword.upsert({
+            where: {
+                email: email,
+            },
+            update: {
+                code: String(token)
+            },
+            create: {
+                email: email,
+                code: String(token)
+            }
+        })
+
+        DEBUG('sr-only', `generated code: ${token} for email: ${email}`)
+
+        return true
+    }catch(err){
+        throw err
+    }
+}
+
+async function recoveryPassword(email, token, password){
+    try{
+
+        const recoveryPassword = await prisma.RecoveryPassword.findUnique({
+            where:{
+                email: email
+            }
+        })
+
+        if(!recoveryPassword) throw new Error('no recovery password request found')
+
+
+        if(recoveryPassword.token !== String(token)) throw new Error('Invalid token')
+
+        password = await cryptPassword(password)
+
+        await prisma.user.update({
+            where:{
+                email: email
+            },
+            data:{
+                password: password
+            }
+        })
+
+        await prisma.RecoveryPassword.delete({
+            where:{
+                email: email
+            }
+        })
+
+        return true
 
     }catch(err){
         throw err
     }
 }
 
-async function cryptPassword(password){
+async function cryptPassword(password) {
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
     return hash;
 }
 
-module.exports = {createNewUser, activeUser}
+module.exports = { createNewUser, activeUser, banUser, unbanUser, generateRecoveryPasswordToken, recoveryPassword }
--- src/server/router/user.js
+++ src/server/router/user.js
@@ -3,32 +3,99 @@ const router = express.Router();
 const DEBUG = require('../middlewares/log')
 const USER = require('../controllers/users')
 
-router.post('/', async (req,res)=>{
-    try{
+
+//create new user
+router.put('/', async (req, res) => {
+    try {
         let user = await USER.createNewUser(req.body)
-        
+
         DEBUG('sr-only', user)
-        
+
         res.json(user)
-    }catch(err){
+    } catch (err) {
         DEBUG('error', err)
         res.status(409).send(String(err))
     }
 })
 
-router.get('/active', async (req,res)=>{
-    try{
+//active pending user
+router.get('/active', async (req, res) => {
+    try {
 
         let active = await USER.activeUser(req.query)
 
-        if(active){
+        if (active) {
             return res.status(200).send(active)
         }
 
-    }catch(err){
+    } catch (err) {
         DEBUG('error', err)
         res.sendStatus(400)
     }
 })
+
+//ban user
+router.post('/ban', async (req, res) => {
+    try {
+
+        let { email } = req.body
+
+        if (!email) throw new Error('email is required')
+
+        if (req.query.unban) {
+            let unban = await USER.unbanUser(email)
+            if (unban) {
+                return res.status(200).send(unban)
+            }
+            return
+        }
+
+        let ban = await USER.banUser(email)
+
+        if (ban) {
+            return res.status(200).send(ban)
+        }
+    } catch (err) {
+        DEBUG('error', err)
+        res.sendStatus(400)
+    }
+})
+
+router.get('/recoveryPassword', async (req, res) => {
+    try {
+
+        let { email, token, password } = req.query
+
+        if (!email) throw new Error('email is required')
+
+        if (!token) {
+
+            //send email with token
+            let recovery = await USER.generateRecoveryPasswordToken(email)
+
+            if (recovery) {
+                return res.status(200).send(recovery)
+            }
+
+        } else {
+            if(!password) throw new Error('password is required')
+           
+            //verify token
+
+            let verify = await USER.recoveryPassword(email, token, password)
+
+            if (verify) {
+                return res.status(200).send(verify)
+            }
+        }
+
+
+    } catch (err) {
+        DEBUG('error', err)
+        res.status(400).send(String(err))
+    }
+})
+
+
 
 module.exports = async (app) => app.use('/user', router)
