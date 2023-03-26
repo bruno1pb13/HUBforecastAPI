b12bf7fe66a95c674cc4e44863985109a961da81
Bruno Santos <bruno1pb13@gmail.com>
2023-03-26 07:08:50 -0300

    last version

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
-
+const cors = require('cors')
 app.use(express.json())
-
+app.use(cors({
+    origin: 'http://192.168.100.29:5173',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}))
 
 let server = {
     info: {
