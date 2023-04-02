12009e50f7adc31321b5fdd8fece6541a9aec00a
Bruno Santos <bruno1pb13@gmail.com>
2023-04-01 23:52:34 -0300

    teste

--- src/server/controllers/sessions.js
+++ src/server/controllers/sessions.js
@@ -3,10 +3,13 @@ const prisma = new PrismaClient()
 var jwt = require('jsonwebtoken');
 
 function newSession(data){
+
+  console.log('newSession', process.env.JWT_SECRET)
+
     return jwt.sign({
         exp: Math.floor(Date.now() / 1000) + (60 * 60),
         data: data
-      }, process.env.JWT_SECRET || 'INSECURE');
+      }, process.env.JWT_SECRET);
 }
 
 module.exports = {newSession}
